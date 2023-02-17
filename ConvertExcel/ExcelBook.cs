c191ef36866dc5d5c25d98feaacc7db6cba6ff77
DFedron <huidong@pdx.edu>
2023-02-17 18:49:25 +0800

    first commit

---
+++ ConvertExcel/ExcelBook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace ConvertExcel
+{
+    public class ExcelBook
+    {
+        private string m_ExcelName;     
+        private List<ExcelSheet> m_Sheets;
+        
+        public ExcelBook(string excelName, List<ExcelSheet> sheets)
+        {
+            m_ExcelName = excelName;
+            m_Sheets = sheets;
+        }
+
+
+        public List<ExcelSheet> GetSheets()
+        {
+            return m_Sheets;
+        }
+
+        public string GetExcelName()
+        {
+            return m_ExcelName;
+        }
+
+    }
+}
---
+++ ConvertExcel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using OfficeOpenXml;    
+using OfficeOpenXml.Drawing.Chart;
+using OfficeOpenXml.Drawing;
+using System.Drawing;
+using OfficeOpenXml.Drawing.Chart.Style;
+
+
+namespace ConvertExcel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "C:\\Users\\wangh\\Desktop\\Access.xlsx";
+            ReadExcel.Instance.ReadFolder(path);
+            //WriteExcel.Instance.WriteToExcel(existingPath, "test");
+        }
+    }
+}
